125d245b099b20dc9fa7130200b5ee4b1f7af078
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-18 14:40:41 +1300

    Created profile component

---
+++ client/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import request from 'superagent'
+
+class Profile extends React.Component {
+  state = {
+
+  }
+
+  componentDidMount () {
+    //this.whateverFunction
+  }
+
+  //whateverFunction
+
+  render () {
+    return (
+      <div className='profile'>
+        <h1 className='profileText'>This is the profile component!</h1>
+      </div>
+    )
+  }
+}
+
+export default Profile
