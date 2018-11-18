29022e000a397628a1dc7bd75fcdd54e9f9166ce
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-18 14:40:57 +1300

    Linked profile component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import request from 'superagent'
 import Weather from './Weather.jsx'
+import Profile from './Profile.jsx'
 
 class App extends React.Component {
   render () {
     return (
       <div>
+        <Profile />
         <Weather />
       </div>
     )
