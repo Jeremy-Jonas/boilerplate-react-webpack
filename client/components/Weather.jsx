13bc2f2edb6ddb5170c4a462457de56ae37225d3
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-22 11:13:58 +1300

    Reactivated API call

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -20,51 +20,51 @@ class Weather extends React.Component {
     }
   }
 
-  // componentDidMount () {
-  //   this.getIP() //start by executing the first function which will get the device's IP
-  // }
+  componentDidMount () {
+    this.getIP() //start by executing the first function which will get the device's IP
+  }
 
-  // getIP () {
-  //   request.get('https://api.ipify.org?format=json') //make the request to the API to return device's IP address
-  //   .then(res => {
-  //     this.setState({
-  //       ip: {
-  //         ipAddress: res.body.ip, //set the IP address in state
-  //         urlIPapi: 'https://geo.ipify.org/api/v1?apiKey=at_SEaLHHzyDOtWBhKfMMh9HiRxcVM1j&ipAddress=' + res.body.ip //append the returned IP address to the URL ready to send to the location API
-  //       }
+  getIP () {
+    request.get('https://api.ipify.org?format=json') //make the request to the API to return device's IP address
+    .then(res => {
+      this.setState({
+        ip: {
+          ipAddress: res.body.ip, //set the IP address in state
+          urlIPapi: 'https://geo.ipify.org/api/v1?apiKey=at_SEaLHHzyDOtWBhKfMMh9HiRxcVM1j&ipAddress=' + res.body.ip //append the returned IP address to the URL ready to send to the location API
+        }
         
-  //     })
-  //     this.getCity() //now run the next function which will use the url created above to request the device's location
-  //   })
-  // }
+      })
+      this.getCity() //now run the next function which will use the url created above to request the device's location
+    })
+  }
 
-  // getCity () {
-  //   request.get(this.state.ip.urlIPapi) //make the request to the API to return device's location
-  //   .then(res => {
-  //     this.setState({
-  //       location: {
-  //         returnedCity: res.body.location.city,
-  //         urlWeatherApi: 'http://api.apixu.com/v1/current.json?key=3beb485786ef46ad9f8223822181311&q=' + res.body.location.city //append the returned device location to the URL ready to send to the weather API
-  //       }
-  //     })
-  //     this.getWeather() //now run the next function which will use the url created above to request the weather for the location
-  //   })
-  // }
+  getCity () {
+    request.get(this.state.ip.urlIPapi) //make the request to the API to return device's location
+    .then(res => {
+      this.setState({
+        location: {
+          returnedCity: res.body.location.city,
+          urlWeatherApi: 'http://api.apixu.com/v1/current.json?key=3beb485786ef46ad9f8223822181311&q=' + res.body.location.city //append the returned device location to the URL ready to send to the weather API
+        }
+      })
+      this.getWeather() //now run the next function which will use the url created above to request the weather for the location
+    })
+  }
 
-  // getWeather () {
-  //   request.get(this.state.location.urlWeatherApi) //make the request to the API to return the weather for the device's current location
-  //     .then(res => {
-  //       this.setState({
-  //         weather:{
-  //           temp: res.body.current.temp_c + '°C',
-  //           condition: res.body.current.condition.text,
-  //           humidity: res.body.current.humidity + "%",
-  //           icon: res.body.current.condition.icon,
-  //           updated: res.body.current.last_updated
-  //         }
-  //       })
-  //     })
-  // }
+  getWeather () {
+    request.get(this.state.location.urlWeatherApi) //make the request to the API to return the weather for the device's current location
+      .then(res => {
+        this.setState({
+          weather:{
+            temp: res.body.current.temp_c + '°C',
+            condition: res.body.current.condition.text,
+            humidity: res.body.current.humidity + "%",
+            icon: res.body.current.condition.icon,
+            updated: res.body.current.last_updated
+          }
+        })
+      })
+  }
 
   render () { //render all the gathered data in the component to display on the site
     return (
