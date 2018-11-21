63b970781ad380472b7f0193f361792f4ec5ff77
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-22 12:22:55 +1300

    Completed joke API call

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
-// import request from 'superagent'
+import request from 'superagent'
 
 class Profile extends React.Component {
-  componentDidMount () {
-    // this.whateverFunction
+  state = {
+    joke: 'Finding a joke...'
   }
 
-  // whateverFunction
+  componentDidMount () {
+    this.getJoke()
+  }
+
+  getJoke () {
+    request.get('https://geek-jokes.sameerkumar.website/api')
+    .then(res => {
+      this.setState({
+        joke: res.body
+      })
+    })
+  }
 
   render () {
     return (
       <div className='profile eight columns'>
         <h4 className='profileText'>This is the profile component!</h4>
         <h5 className='profileText'>Here's a joke while you wait...</h5>
-        <h5 className='profileText'></h5>
+        <h5 className='profileText'>{this.state.joke}</h5>
       </div>
     )
   }
 }
 
 export default Profile
-
-// https://geek-jokes.sameerkumar.website/api
