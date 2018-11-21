e5c4cb81cf55a99faa0f78a9f1078c53d3fc78b1
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-22 11:57:31 +1300

    Adding joke as placeholder

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -12,9 +12,13 @@ class Profile extends React.Component {
     return (
       <div className='profile eight columns'>
         <h4 className='profileText'>This is the profile component!</h4>
+        <h5 className='profileText'>Here's a joke while you wait...</h5>
+        <h5 className='profileText'></h5>
       </div>
     )
   }
 }
 
 export default Profile
+
+// https://geek-jokes.sameerkumar.website/api
