f85c7c0d578d1c9c9d48395fe006e7ed9032308f
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-28 15:14:54 +1300

    Removed joke API call & render

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -3,28 +3,15 @@ import request from 'superagent'
 
 class Profile extends React.Component {
   state = {
-    joke: 'Finding a joke...'
   }
 
-  componentDidMount () {
-    this.getJoke()
-  }
-
-  getJoke () {
-    request.get('https://geek-jokes.sameerkumar.website/api')
-    .then(res => {
-      this.setState({
-        joke: res.body
-      })
-    })
+  componentDidMount () {  
   }
 
   render () {
     return (
       <div className='profile eight columns'>
         <h4 className='profileText'>This is the profile component!</h4>
-        <h5 className='profileText'>Here's a joke while you wait...</h5>
-        <h5 className='profileText'>{this.state.joke}</h5>
       </div>
     )
   }
